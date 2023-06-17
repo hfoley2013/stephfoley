46e544e23a05df3492951b8307ddabbd78c7b131
Harper Foley <harper.e.foley@gmail.com>
2023-06-17 02:00:41 -0700

    Updates revalidation path to account for authors

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,19 +1,76 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { revalidatePath } from 'next/cache'
+import { client } from '@/sanity/lib/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createClient, groq, type SanityClient } from 'next-sanity'
+import { type ParseBody, parseBody } from 'next-sanity/webhook'
 
-export default async function revalidateBlogPosts(req: NextApiRequest, res: NextApiResponse) {
-  const { slug } = req.query
-  const path = `/blog/${slug}`
+export { config } from 'next-sanity/webhook'
 
-  // Validate the secret key
-  const secretKey = process.env.SANITY_REVALIDATE_SECRET
-  const requestSecretKey = req.headers['x-sanity-revalidation-secret-key']
-  if (secretKey !== requestSecretKey) {
-    return res.status(401).json({ error: 'Unauthorized' })
+export default async function revalidate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { body, isValidSignature } = await parseBody(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET
+    )
+    if (isValidSignature === false) {
+      const message = 'Invalid signature'
+      console.log(message)
+      return res.status(401).send(message)
+    }
+
+    const { _type, _id } = body
+
+    if (typeof _id !== 'string' || !_id) {
+      const invalidId = 'Invalid _id'
+      console.error(invalidId, { body })
+      return res.status(400).send(invalidId)
+    }
+
+    const staleRoutes = await queryStaleRoutes(_type, _id)
+    await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
+
+    const updatedRoutes = `Updated routes: ${staleRoutes.join(', ')}`
+    console.log(updatedRoutes)
+    return res.status(200).send(updatedRoutes)
+  } catch (err: any) {
+    console.error(err)
+    return res.status(500).send(err.message)
+  }
+}
+
+type StaleRoute = '/' | `/blog/${string}`
+
+async function queryStaleRoutes(
+  type: string,
+  id: string
+): Promise<StaleRoute[]> {
+
+  if (type === 'post') {
+    const exists = await client.fetch(groq`*[_id == $id][0]`, { id })
+    if (!exists) {
+      const slug = await client.fetch(groq`*[_id == $id].slug.current`, { id })
+      const staleRoutes: StaleRoute[] = []
+      if (slug) {
+        staleRoutes.push(`/blog/${slug}`)
+      }
+      return staleRoutes
+    }
   }
 
-  // Revalidate the specified path
-  revalidatePath(path)
+  if (type === 'author') {
+    const slugs = await client.fetch(
+      groq`*[_type == "author" && _id == $id] {
+      "slug": *[_type == "post" && references(^._id)].slug.current
+    }["slug"][]`,
+      { id }
+    )
 
-  return res.status(200).json({ revalidated: true, now: Date.now() })
+    if (slugs.length > 0) {
+      return ['/', ...slugs.map((slug: string) => `/blog/${slug}`)]
+    }
+  }
+
+  return []
 }
