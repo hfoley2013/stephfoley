75e3f7e21a8de0126508a64b1ab18ed9aa1119e1
Harper Foley <harper.e.foley@gmail.com>
2023-06-16 20:27:08 -0700

    Fixes og & twitter image rendering issues.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: [
+      'cdn.sanity.io',
+      'stephfoley.com',
+      'localhost',
+    ],
   }
 }
 
